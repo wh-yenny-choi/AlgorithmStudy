f34154acaa2b5c132bf0ad12868a4eb3f9967ee4
yenny <choiwonhee.data@gmail.com>
2021-06-05 23:21:21 +0900

    topological sorting 위상정렬

---
+++ 최적의 코딩을 결정하는 기본 알고리즘/16.py
@@ -0,0 +1,39 @@
+from collections import deque  # 별로 라이브러리 호출
+
+# 노드의 개수와 간선의 개수를 입력 받기
+v, e = map(int, input().split())
+# 모든 노드에 대한 진입차수는 0으로 초기화
+indegree = [0] * (v + 1)
+# 각 노드에 연결된 간선 정보를 담기 위한 연결 리스트 초기화
+graph = [[] for i in range(v + 1)]  # 전체 노드 개수 v개
+
+# 방향 그래프의 모든 간선 정보를 입력 받기
+for _ in range(e):
+    a, b = map(int, input().split())
+    graph[a].append(b)  # 정점 A에서 B로 이동 가능 = B에 대해 들어오는 간선 존재
+    # 진입 차수를 1 증가
+    indegree[b] += 1
+
+# 위상 정렬 함수
+def toplogy_sort():
+    result = []  # 알고리즘 수행 결과를 담을 리스트
+    q = deque()  # 큐 기능을 위한 deque 라이브러리 사용
+    # 처음 시작할 때는 진입차수가 0인 노드를 큐에 삽입
+    for i in range(1, v + 1):
+        if indegree[i] == 0:
+            q.append(i)
+    # 큐가 빌 때까지 반복
+    while q:
+        # 큐에서 원소 꺼내기
+        now = q.popleft()
+        result.append(now)  # q에 들어간 순서 = 전체 위상 정렬 수행 결과 >> q에서 원소를 꺼낼때마다 결과 리스트에 담기
+        # 해당 원소와 연결된 노드들의 진입차수에서 1빼기
+        for i in graph[now]:
+            indegree[i] -= 1  # 나가는 간선 제거
+            # 새롭게 진입차수가 0이 되는 노드를 큐에 삽입
+            if indegree[i] == 0:
+                q.append(i)
+    # 위상 정렬을 수행한 결과 출력
+    for i in result:
+        print(i, end=' ')  # 결과 리스트에 담긴 각각의 원소 출력력
+toplogy_sort()
