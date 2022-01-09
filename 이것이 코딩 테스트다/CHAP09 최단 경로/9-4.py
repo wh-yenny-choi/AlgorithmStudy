1fd87a5d9c416e96a391ea7a2a772ae49738c942
yenny <choiwonhee.data@gmail.com>
2022-01-09 20:17:02 +0900

    chap09 최단 거리

---
+++ 이것이 코딩 테스트다/CHAP09 최단 경로/9-4.py
@@ -0,0 +1,64 @@
+'''
+K번의 회사를 거쳐 X번의 회사로 가는 최소 이동 시간 출력
+만약 X번 회사에 도달할 수 없다면 -1 출력
+
+입력 예시 1
+----------------
+5 7  # 회사의 개수 N, 경로의 개수 M
+1 2
+1 3
+1 4
+2 4
+3 4
+3 5
+4 5
+4 5  # X와 K
+----------------
+출력 예시 1 : 3
+
+입력 예시 2
+----------------
+4 2
+1 3
+2 4
+3 4
+----------------
+출력 예시 2 : -1
+'''
+INF = int(1e9)  # 무한을 의미하는 값으로 10억을 설정
+
+# 노드의 개수 및 간선의 개수 입력받기 (회사의 개수 N, 경로의 개수 M)
+n, m = map(int, input().split())
+# 2차원 리스트(그래프 표현)만들고, 모든 값을 무한으로 초기화
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+# 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
+
+# 각 간선에 대한 정보를 입력받아, 그 값으로 초기화
+for _ in range(m):
+    # A와 B가 서로에게 가는 비용은 1이라고 설정
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+# 거쳐 갈 노드 X와 최종 목적지 노드 K를 입력받기
+x, k = map(int, input().split())
+
+# 점화식에 따라 플로이드 워셜 알고리즘을 수행
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+# 수행된 결과를 출력
+distance = graph[1][k] + graph[k][x]
+
+# 도달할 수 없는 경우 INF출력
+if distance >= INF:
+    print("-1")
+else:
+    print(distance)
